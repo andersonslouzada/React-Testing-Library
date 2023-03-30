b0564feec2744a38fa76c39b09b50c65f8b104dd
andersonlouzada <anderson.s.louzada@gmail.com>
2023-03-30 16:59:06 -0300

    cria teste para componente NotFound comforme solicitado no requisito 4

--- src/tests/NotFound.test.js
+++ src/tests/NotFound.test.js
@@ -1,1 +1,23 @@
-test('', () => {});
+import React from 'react';
+import { screen } from '@testing-library/react';
+import renderWithRouter from '../renderWithRouter';
+import { NotFound } from '../pages';
+
+describe('Testa o componente NotFound.js', () => {
+  it('Testa se a página contém um heading h2 com o texto Page requested not found', () => {
+    renderWithRouter(<NotFound />);
+    const notFoundTitle = screen.getByRole('heading', {
+      name: 'Page requested not found',
+
+    });
+    expect(notFoundTitle).toBeInTheDocument();
+  });
+
+  it('Teste se a página mostra a imagem "https://media.giphy.com/media/kNSeTs31XBZ3G/giphy.gif"', () => {
+    renderWithRouter(<NotFound />);
+    const notFoundImg = screen.getByRole('img', {
+      name: /pikachu crying because the page requested was not found/i,
+    });
+    expect(notFoundImg.src).toBe('https://media.giphy.com/media/kNSeTs31XBZ3G/giphy.gif');
+  });
+});
